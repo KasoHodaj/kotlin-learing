ed7efa604cc0959e6370d57aaedeb61aa9a7d26e
kaso hodaj <kasohodaj@yahoo.gr>
2025-03-02 12:16:03 +0200

    control_flow_pk commit

--- src/ii_control_flow/IfElse.kt
+++ src/ii_control_flow/IfElse.kt
@@ -1,2 +1,31 @@
 package ii_control_flow
 
+// If expression
+fun main(){
+    // In kotlin there is not ternary operator
+    // If as a STATEMENT
+    var max = 10;
+    if(max < 20){
+        max = 20;
+    }
+    println("max: $max");
+
+    // If else
+    if(10 < 5)
+        println("True");
+    else
+        println("False");
+
+    var a  = 5
+    var b = 7
+
+    // If as an EXPRESSION ~ similar to ternary operator and less confusing with long conditions
+    max = if(a > b) a else b;
+    println("max: $max")
+
+    // else if as an EXPRESSION
+    max = if(a == b) 0 else if(a < b) b else if(a > b) a else -1
+    println("max: $max");
+
+
+}
--- src/ii_control_flow/WhenExpression.kt
+++ src/ii_control_flow/WhenExpression.kt
@@ -1,2 +1,63 @@
 package ii_control_flow
 
+// When expressions and statements
+
+fun main(){
+    // 'when' is a conditional expression that runs code based on multiple possible values or conditions.
+    // It is similar to the switch statement in Java, C, and similar languages. For example:
+
+    /**
+     *  Firstly, you can use 'when' either as an expression or as a statement.
+     *  As an expression, 'when' returns a value for later use in your code.
+     *  As a statement, 'when' completes an action without returning anything of further use.
+     */
+
+    // 'when' as a STATEMENT
+    val x = 1;
+    when (x){
+        1 -> println("x = 1");
+        2 -> println("x = 2");
+        else -> println("x is neither 1 or 2");
+    }
+
+    // 'when' as an EXPRESSION
+    val msg = when (x){
+        1 -> "x = 1";
+        2 -> "x = 2";
+        else -> "x is neither 1 or 2";
+    }
+    println("msg: $msg");
+
+    when (x) {
+        0, 1 -> println("x == 0 or x == 1")
+        else -> println("otherwise")
+    }
+
+    when (x){
+        0.toInt() -> println("0 encodes x");
+        else -> println("0 does not encode x");
+    }
+
+    when (x) {
+        in 1..10 -> println("x is in the range of [1,10]");
+        !in 1..10 -> println("x is outside the range [1,10]");
+        else -> println("None of the above");
+    }
+
+    val flag = "kotlin"
+    fun hasPrefix(flag: Any) = when (flag){
+        is String -> println(flag.startsWith("k")); // true
+        else -> println("False") //false
+    }
+    hasPrefix(flag); // -> false
+
+    when {
+        isOdd(x) -> println("x is odd");
+        !isOdd(x) -> println("x is even");
+        else -> println("None of the above");
+    }
+}
+
+fun isOdd(x: Int): Boolean {
+    return x % 2 != 0 // Return true if x is odd, otherwise false
+}
